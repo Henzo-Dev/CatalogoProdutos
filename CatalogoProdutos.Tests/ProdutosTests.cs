1d156010cedeb860ec1bccc2e7b5cbcda06bc9ba
49087790821 <49087790821@ECE429N10SN1>
2026-09-24 15:28:03 -0300

    Adicionar arquivos de projeto.

---
+++ CatalogoProdutos.Tests/ProdutosTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace CatalogoProdutos.Tests
+{
+    public class ProdutosTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProdutosTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task PaginaProdutos_DeveRetornarSucesso()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task PaginaProdutos_DeveRetornarCodigo200()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PaginaProdutos_DeveConterTitulo()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos");
+
+            var conteudo = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Catálogo de Produtos", conteudo);
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Produtos")]
+        [InlineData("/Home/Privacy")]
+        public async Task Paginas_DeveRetornarSucesso(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task PaginaInexistente_DeveRetornar404()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos/ABC");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PaginaProdutos_DeveConterNotebook()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos");
+
+            var conteudo = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Notebook", conteudo);
+        }
+
+        [Fact]
+        public async Task PaginaProdutos_DeveRetornarHTML()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos");
+
+            Assert.Equal(
+                "text/html",
+                response.Content.Headers.ContentType?.MediaType);
+        }
+
+        [Fact]
+        public async Task PaginaCreate_DeveConterCadastrarProduto()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/Produtos/Create");
+
+            var conteudo = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.Contains("Cadastrar Produto", conteudo);
+        }
+    }
+}
---
+++ CatalogoProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoProdutos.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
